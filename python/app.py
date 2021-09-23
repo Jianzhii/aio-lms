903d05f8b442da6eb28dbe18c516312e52426acd
Jianzhii <60025391+Jianzhii@users.noreply.github.com>
2021-09-23 23:56:16 +0800

    added structure for python backend files

---
+++ python/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+import login ## import the name of the file containing that module
+
+@app.route("/db_port", methods=['GET'])
+def test():
+
+    return jsonify(
+        {
+            "code": 200,
+            "data": os.environ.get("DB_PORT")
+        }
+    ), 200
+
+app.run(host='0.0.0.0', port=8000, debug=True)
