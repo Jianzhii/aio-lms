ae0d18f5414ed4e7bbc070597c935f99d025073a
Jianzhii <60025391+Jianzhii@users.noreply.github.com>
2021-09-25 18:07:26 +0800

    test api call in index

--- python/app.py
+++ python/app.py
@@ -1,9 +1,12 @@
 import os
+
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
+CORS(app)
+
 db_host = os.environ.get("DB_HOSTNAME")
 db_port = os.environ.get("DB_PORT")
 db_username = os.environ.get("DB_USERNAME")
@@ -12,6 +15,7 @@ db_password = os.environ.get("DB_PASSWORD")
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}:{db_port}/lms"
 db = SQLAlchemy(app)
 import dbsample  # # import the name of the file containing that module
+
 
 @app.route("/test", methods=['GET'])
 def test():
