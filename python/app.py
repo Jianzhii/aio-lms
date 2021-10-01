fb16698a4f98d43bbb6bd2d3367c4b8fb6dfe82a
Jianzhii <60025391+Jianzhii@users.noreply.github.com>
2021-10-02 01:03:37 +0800

    courses CRUD apis

--- python/app.py
+++ python/app.py
@@ -11,10 +11,12 @@ db_host = os.environ.get("DB_HOSTNAME")
 db_port = os.environ.get("DB_PORT")
 db_username = os.environ.get("DB_USERNAME")
 db_password = os.environ.get("DB_PASSWORD")
+print(db_port)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}:{db_port}/lms"
 db = SQLAlchemy(app)
 import dbsample  # # import the name of the file containing that module
+import course
 
 @app.route("/test", methods=['GET'])
 def test():
@@ -25,5 +27,6 @@ def test():
             "data": "test"
         }
     ), 200
-    
+
+os.environ["FLASK_RUN_FROM_CLI"] = "false"
 app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ python/course.py
@@ -0,0 +1,140 @@
+from app import app, db
+from flask import jsonify, request
+
+class Course(db.Model):
+
+    __tablename__ = 'course'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
+
+# Get all Courses
+@app.route("/all_course", methods=['GET'])
+def getAll():
+    courses = Course.query.all()
+    return jsonify(
+        {
+            "code": 200,
+            "data": [course.json() for course in courses]
+        }
+    ), 200
+
+# Get one course
+@app.route("/course/<int:id>", methods=['GET'])
+def getOne(id):
+    course = Course.query.filter_by(id=id).first()
+    if course:
+        return jsonify(
+            {
+                "code": 200,
+                "data": course.json()
+            }
+        ), 200
+    else:
+        return jsonify(
+            {
+                "code":404,
+                "data": {
+                    "id": id
+                },
+                "message": "Course not found."
+            }
+        )
+
+# Add one course
+@app.route("/course", methods=['POST'])
+def addCourse():
+    data = request.get_json()
+    course = Course(**data)
+    try:
+        db.session.add(course)
+        db.session.commit()
+    except Exception as e:
+        return jsonify(
+            {
+                "code":500,
+                "message": f"An error occurred while creating course: {e}"
+            }
+        )
+    
+    return jsonify(
+        {
+            "code": 200,
+            "data": course.json()
+        }
+    ), 200
+
+#  Update Course
+@app.route("/course", methods=['PUT'])
+def updateCourse():
+    try:
+        data = request.get_json()
+        id = data['id']
+        course = Course.query.filter_by(id=id).first()
+        if not course:
+            return jsonify(
+                {
+                    "code":404,
+                    "data": {
+                        "id": id
+                    },
+                    "message": "Course not found."
+                }
+            )
+        course.name = data['name']
+        course.description = data['description']
+        db.session.commit()
+        
+        return jsonify(
+            {
+                "code": 200,
+                "data": data,
+                "message": "Course successfully updated"
+            }
+        )
+
+    except Exception as e:
+        return jsonify(
+            {
+                "code":500,
+                "message": f"An error occurred while updating course: {e}"
+            }
+        )
+
+# Delete course
+@app.route("/course/<int:id>", methods=['DELETE'])
+def deleteCourse(id):
+    course = Course.query.filter_by(id=id).first()
+    if not course:
+        return jsonify(
+            {
+                "code":404,
+                "data": {
+                    "id": id
+                },
+                "message": "Course not found."
+            }
+        )
+    try:
+        db.session.delete(course)
+        db.session.commit()
+    except Exception as e: 
+        return jsonify( 
+            {
+                "code":500,
+                "message": f"An error occurred while deleting course: {e}"
+            }
+        )
+    return jsonify(
+        {
+            "code": 200,
+            "message": "Course successfully deleted"
+        }
+    )
