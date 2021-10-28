62a5b970dfaa228ce59a3326813bce727bc31afc
Jianzhii <60025391+Jianzhii@users.noreply.github.com>
2021-10-28 20:25:36 +0800

    test click image

--- ui/js/common.js
+++ ui/js/common.js
@@ -2,3 +2,8 @@ function logout() {
     sessionStorage.clear();
     window.location.href = '../../index.html';
 }
+
+
+function adminHome() {
+    window.location.href = '../admin/admin_home.html';
+}
