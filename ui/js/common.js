4d0eba0a8ce0258fd27b2d2a3e47f5c31a5f2609
Jianzhii <60025391+Jianzhii@users.noreply.github.com>
2021-10-28 20:30:05 +0800

    image to bring user to home

--- ui/js/common.js
+++ ui/js/common.js
@@ -3,7 +3,14 @@ function logout() {
     window.location.href = '../../index.html';
 }
 
-
 function adminHome() {
     window.location.href = '../admin/admin_home.html';
+}
+
+function trainerHome() {
+    window.location.href = '../trainer/trainer_home.html';
+}
+
+function learnerHome() {
+    window.location.href = '../learner/learner_home.html';
 }
