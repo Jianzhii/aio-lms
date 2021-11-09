6b2c6c210491a7db68c1829f6511c2176cbf6a4d
javieryw <72239272+javieryw@users.noreply.github.com>
2021-11-09 17:21:49 +0800

    test case on enrolment_request

--- python/tests/user_test.py
+++ python/tests/user_test.py
@@ -47,3 +47,52 @@ def test_get_trainer():
     with app.test_client() as test_client:
         response = test_client.get('/all_trainer')
         assert response.status_code == 200
+
+##############################
+####### Javier's Part ########
+##############################
+
+### View All Pending  Enrolment Requests ###
+def test_pending_enrolment_request():
+    with app.test_client() as test_client:
+        response = test_client.get('/enrolment_request')
+        assert response.status_code == 200 
+
+### View Pending Enrolment Requests by Learners ID ###
+def test_learner_enrolment_request():
+    with app.test_client() as test_client:
+        response = test_client.get('/enrolment_request/learner/6')
+        assert response.status_code == 200
+
+### Creation of Enrolment Requests with All Conditions Satisfied ###
+def test_create_enrolment_request():
+    with app.test_client() as test_client:
+        body =\
+        {
+            "user_id" : 17,
+            "group_id":91
+        }
+        response = test_client.post('/enrolment_request',json=body)
+        assert response.status_code == 200
+### Check Creation of Enrolment Request if not within enrolment period of the group ###
+def test_check_enrolment_period():
+    with app.test_client() as test_client:
+        body =\
+        {
+            "user_id" : 15,
+            "group_id":1
+        }
+        response = test_client.post('/enrolment_request',json=body)
+        assert response.status_code == 406
+
+### Check creation of enrolment request if group size is ###
+def test_check_group_size():
+    with app.test_client() as test_client:
+        body =\
+        {
+            "user_id" : 14,
+            "group_id":88
+        }
+        response = test_client.post('/enrolment_request',json=body)
+        assert response.status_code == 406
+
