f60be7076f041afdc3e5a57176dca803029436bf
ruolin22 <ruolin.chua.2019@sis.smu.edu.sg>
2021-11-11 01:02:32 +0800

    delete testing

--- python/testing.py
+++
@@ -1,9 +0,0 @@
-ylist = [5,6,6,14,15,16,18,28,29,35,35,35,36,37,38,38,42,50,50,60,60,58,75,75,85,90,110,328]
-
-target = 200
-
-for i in range(len(ylist)):
-    sno = target-ylist[i]
-    for j in range(i+1,len(ylist)):
-        if ylist[j] == sno:
-            print(ylist[i],ylist[j])
